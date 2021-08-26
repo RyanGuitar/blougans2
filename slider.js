f773d04975a60b11b85f372d4100a027e39e9a2e
Ryan Els <39189847+RyanGuitar@users.noreply.github.com>
2021-08-26 02:40:39 +0200

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,19 @@
+import {
+  images
+} from '../data/images.js'
+
+function Slider() {
+  let template = '';
+  for (let {
+    name, title
+  } of images) {
+    template += `
+    <div class="padding">
+    <img src=${name} alt=${name} class="sticker-gallery" loading="lazy">
+    </div>`
+
+  }
+  return template;
+}
+
+export default Slider()
