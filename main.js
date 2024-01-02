006fd0be542e0e2d13df094c21f2fe47c5b2e231
Ryan Els <39189847+RyanGuitar@users.noreply.github.com>
2024-01-02 16:38:42 +0200

    Changed prices

--- main.js
+++ main.js
@@ -119,8 +119,8 @@ function addSlider(){
 
 function addRoomPrices(){
   const roomPrices_html = `
-      <span>Single R850</span>
-      <span>Sharing R650 pp</span>
+      <span>Single R950</span>
+      <span>Sharing R750 pp</span>
   `
   const prices = document.querySelectorAll('.roomPrices')
   prices.forEach(price => {
